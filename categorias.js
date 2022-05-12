13e89e242516e9cdad3c4be7a24f5b33a7d8cd84
Alexa Trifilio <alexatrifilio@gmail.com>
2022-05-12 16:50:13 -0300

    began creating categorias form

--- categorias.js
+++ categorias.js
@@ -3,7 +3,8 @@
 cardCreator('Categorias', mainContainer);
 const categoriasCard = document.getElementById('categorias');
 
-// creo la variable cartegoriasCard para poder hacerle los appends a ese elemento. El título lo puse sin tilde para que no se agregue con tilde en el ID, pero habria que preguntar como podríamos resolver eso y también en el caso de que sean dos palabras unidas por - . yo prefiero irlas poniendo "feas" en los titulos y despues solucionarlo dentro de la funcioncreadoa de cards, porque creo que con algun metodo de string podemos acomodar lo que se ve :)
+formCreator(['Nombre'], 'text', categoriasCard, '', 'categorias-form');
+
 
 
 
