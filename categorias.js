5b47d2b1afd40e81baa34684368593d23bd7e3a8
Alexa Trifilio <alexatrifilio@gmail.com>
2022-05-14 17:32:20 -0300

    initial and new categories added to local storage

--- categorias.js
+++ categorias.js
@@ -40,58 +40,109 @@ categoriasCard.appendChild(catFields);
 
 
 // - Categories in local storage - //
-const initialCate = ['Comida', 'Servicios','Trabajo', 'Salidas', 'Educación', 'Transporte'];
-localStorage.setItem('categories', JSON.stringify(initialCate)); 
+
+const idKeys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz';
+const randomId = ()=> {
+    let finalId = '';
+    for(let i = 0; i < 9; i++){
+        const random = Math.floor(Math.random()*idKeys.length);
+        let elem = idKeys.slice(random, random+1);
+        finalId = finalId.concat(elem);
+    }
+    return finalId
+}
+
+const initialCategories = [{
+    id: `${randomId()}`,
+    cat: 'Comida'
+},
+{
+    id: `${randomId()}`,
+    cat: 'Servicios'
+},
+{
+    id: `${randomId()}`,
+    cat: 'Salidas'
+},
+{
+    id: `${randomId()}`,
+    cat: 'Educación'
+},
+{
+    id: `${randomId()}`,
+    cat: 'Transporte'
+},
+{
+    id: `${randomId()}`,
+    cat: 'Trabajo'
+},
+]
+
+
+
+const initCat = () => {
+    const storage = JSON.parse(localStorage.getItem('ahorradas-data'));
+
+        if(storage.categories.length === 0){
+            for (let elem of initialCategories){
+                storage.categories.push(elem)
+            }
+            localStorage.setItem('ahorradas-data', JSON.stringify(storage))
+        }
+
+
+};
+
+initCat()
 
     // -- Show categories -- //
 
 const showCategories = () => {
-    const categories = JSON.parse(localStorage.getItem('categories'));
+    const lsdata = JSON.parse(localStorage.getItem('ahorradas-data'));
 
-    categories.forEach((cat)=> {
-        let lowerCaseCat = cat.toLowerCase();
-        let row = document.createElement('div');
-        row.classList.add('row', 'p-3');
-        let col1 = document.createElement('div');
-        col1.classList.add('col', 'col-9');
-        let col2 = document.createElement('div');
-        col2.classList.add('col', 'col-3', 'd-flex','align-items-center');
-        let span = document.createElement('span');
-        span.appendChild(document.createTextNode(cat));
-        span.classList.add('d-inline-block','bg-info', 'bg-opacity-50', 'p-1');
-        col1.appendChild(span);
-        let editar = document.createElement('a');
-        editar.setAttribute('href', './editar-categoria.html');
-        editar.classList.add('me-2');
-        editar.appendChild(document.createTextNode('Editar'));
-        col2.appendChild(editar);
-        let eliminar = document.createElement('a');
-        eliminar.setAttribute('href', '#');
-        eliminar.setAttribute(`data-${lowerCaseCat}`, lowerCaseCat);
-        eliminar.appendChild(document.createTextNode('Eliminar'));
-        col2.appendChild(eliminar);
-        row.appendChild(col1);
-        row.appendChild(col2);
-
-        catFields.appendChild(row);
-
-        // -- Eliminar event -- //
-
-        eliminar.addEventListener('click', (e)=> {
-            const lsCategories = JSON.parse(localStorage.getItem('categories'));
-            console.log('holi');
-            lsCategories.forEach((ct) => {
-                const lowerCaseCt = ct.toLowerCase()
-                console.log(e.target.querySelector([`data-${lowerCaseCt}="${lowerCaseCt}"`]));
-                if(e.target.querySelector(`data-${lowerCaseCt}`).includes(lowerCaseCt)){
-                    lsCategories.splice(parseInt(index),1);
-                    console.log(lsCategories);
-                }
-            })
-        })
-
-    })
+    lsdata.categories.forEach(cat =>{
+        const newCat = cat.cat;
+            let row = document.createElement('div');
+            row.classList.add('row', 'p-3');
+            let col1 = document.createElement('div');
+            col1.classList.add('col', 'col-9');
+            let col2 = document.createElement('div');
+            col2.classList.add('col', 'col-3', 'd-flex','align-items-center');
+            let span = document.createElement('span');
+            span.appendChild(document.createTextNode(newCat));
+            span.classList.add('d-inline-block','bg-info', 'bg-opacity-50', 'p-1');
+            col1.appendChild(span);
+            let editar = document.createElement('a');
+            editar.setAttribute('href', `./editar-categoria.html?id=${cat.id}`);
+            editar.classList.add('me-2');
+            editar.appendChild(document.createTextNode('Editar'));
+            col2.appendChild(editar);
+            let eliminar = document.createElement('a');
+            eliminar.setAttribute('href', '#');
+            eliminar.appendChild(document.createTextNode('Eliminar'));
+            col2.appendChild(eliminar);
+            row.appendChild(col1);
+            row.appendChild(col2);
     
+            catFields.appendChild(row);            
+
+
+            // -- Eliminar event -- //
+        
+        // eliminar.addEventListener('click', (e)=> {
+        //     const lsCategories = JSON.parse(localStorage.getItem('categories'));
+        //     console.log('holi');
+        //     lsCategories.forEach((ct) => {
+        //         const lowerCaseCt = ct.toLowerCase()
+        //         console.log(e.target.querySelector([`data-${lowerCaseCt}="${lowerCaseCt}"`]));
+        //         if(e.target.querySelector(`data-${lowerCaseCt}`).includes(lowerCaseCt)){
+        //             lsCategories.splice(parseInt(index),1);
+        //             console.log(lsCategories);
+        //         }
+        //     })
+        // })
+
+    })    
 }
 
 showCategories();
@@ -100,11 +151,11 @@ showCategories();
 
     categoriasForm.addEventListener('submit', (e)=>{
         e.preventDefault();
-        const lsCategories = JSON.parse(localStorage.getItem('categories'));
-        const newCat = e.target.querySelector('#nombre-cat').value;
-        lsCategories.push(newCat);
-        
-        localStorage.setItem('categories', JSON.stringify(lsCategories));
+        const lsData = JSON.parse(localStorage.getItem('ahorradas-data'));
+        const newCatName = e.target.querySelector('#nombre-cat').value;
+        const newCat = {id:`${randomId()}`, cat: `${newCatName}`};
+        lsData.categories.push(newCat);
+        localStorage.setItem('ahorradas-data', JSON.stringify(lsData));
         
         catFields.innerHTML = "";
         
