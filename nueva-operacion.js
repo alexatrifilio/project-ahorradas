d2b36e872b59b1b804e569d44bf8246bd7878b27
Vanessa Neira <vaneira24@gmail.com>
2022-05-16 20:42:21 -0300

    Button to save nueva-operacion in transactions

--- nueva-operacion.js
+++ nueva-operacion.js
@@ -1,13 +1,12 @@
 // --- CARD --- //
 
-cardCreator('Nueva-operacion', mainContainer);
+cardCreator('nueva-operacion', mainContainer);
 const nuevaOpCard = document.getElementById('nueva-operacion');
 
 const nuevaOpForm = document.createElement('form');
 nuevaOpForm.setAttribute('id', 'nueva-operacion-form');
 nuevaOpForm.setAttribute('class', 'form-group');
 nuevaOpCard.appendChild(nuevaOpForm);
-
 
 function createInputField(formId, type, name, placeholder, required){
     const form = document.getElementById(formId);
@@ -20,9 +19,9 @@ function createInputField(formId, type, name, placeholder, required){
     input.setAttribute('name', name);
     input.setAttribute('placeholder', placeholder);
     input.setAttribute('required', required);
+    input.setAttribute('id', `input-${name}`);
     form.appendChild(label);
     form.appendChild(input);
-
 }
 
 function createSelectField(formId, name, options){
@@ -36,6 +35,7 @@ function createSelectField(formId, name, options){
     select.setAttribute('name', name);
     option.appendChild(document.createTextNode('Seleccione una opción'));
     select.appendChild(option);
+    select.setAttribute('id', `select-${name}`);
     for (let i in options){
         const option = document.createElement('option');
         option.appendChild(document.createTextNode(options[i]));
@@ -47,20 +47,54 @@ function createSelectField(formId, name, options){
 
 fieldNames = ['description', 'amount', 'type', 'category', 'date']
 
-
 createInputField('nueva-operacion-form', 'text', 'description', '', true);
 createInputField('nueva-operacion-form', 'number', 'amount', '', true);
 createSelectField('nueva-operacion-form', 'type', ['Ingreso', 'Gasto']);
 createSelectField('nueva-operacion-form', 'category', ['Entretenimiento', 'Salud', 'Renta', 'Otros']);
 createInputField('nueva-operacion-form', 'date', 'date', '', true);
 
+// -- get the table transactions from local storage --//
+
+const transactions = JSON.parse(window.localStorage.getItem('transactions'));
+console.log(transactions);
+
+// Create a button to submit the form and save the info in an array in the local storage
+
+const submitButton = document.createElement('button');
+submitButton.setAttribute('type', 'submit');
+submitButton.setAttribute('class', 'btn btn-primary');
+submitButton.appendChild(document.createTextNode('Guardar'));
+nuevaOpForm.appendChild(submitButton);
 
 
+// Create a function to save the info in an array in the local storage
 
+function saveOperation(e){
+    e.preventDefault();
+    const description = document.getElementById('input-description').value;
+    const amount = document.getElementById('input-amount').value;
+    const type = document.getElementById('select-type').value;
+    const category = document.getElementById('select-category').value;
+    const date = document.getElementById('input-date').value;
 
+    const newTransaction = {
+        description,
+        amount,
+        type,
+        category,
+        date
+    }
 
+    transactions.push(newTransaction);
+    window.localStorage.setItem('transactions', JSON.stringify(transactions));
+    console.log(transactions);
+}
 
+// Add an event listener to the submit button to save the info in an array in the local storage
 
+submitButton.addEventListener('click', saveOperation);
+
+// Create a function to show the info in the table
 
 
 
